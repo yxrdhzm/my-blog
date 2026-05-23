4c051f25e6bb5ce5a9f75082c4b65967c8d9f15d
yxrdhzm <159686676+yxrdhzm@users.noreply.github.com>
2026-05-23 15:36:26 +0800

    改域名

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "个人博客",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://h.yxrd.dpdns.org",
 
 	// 站点描述
 	description:
